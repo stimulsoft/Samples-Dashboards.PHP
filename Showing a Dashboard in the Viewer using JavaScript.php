a0c4b5ed16bcb21ea787ac64b38c0c6681401192
VladimirHot <hotchenko@gmail.com>
2022-12-13 15:20:31 +0300

    change resources path

--- Showing a Dashboard in the Viewer using JavaScript.php
+++ Showing a Dashboard in the Viewer using JavaScript.php
@@ -18,9 +18,9 @@ use Stimulsoft\StiHandler;
     </style>
 
     <!-- https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_deployment.htm -->
-    <script src="/vendor/stimulsoft/reports-php/public/scripts/stimulsoft.reports.js" type="text/javascript"></script>
-    <script src="/vendor/stimulsoft/dashboards-php/public/scripts/stimulsoft.dashboards.js" type="text/javascript"></script>
-    <script src="/vendor/stimulsoft/reports-php/public/scripts/stimulsoft.viewer.js" type="text/javascript"></script>
+    <script src="/vendor/stimulsoft/reports-php/scripts/stimulsoft.reports.js" type="text/javascript"></script>
+    <script src="/vendor/stimulsoft/dashboards-php/scripts/stimulsoft.dashboards.js" type="text/javascript"></script>
+    <script src="/vendor/stimulsoft/reports-php/scripts/stimulsoft.viewer.js" type="text/javascript"></script>
 
     <script type="text/javascript">
         <?php
